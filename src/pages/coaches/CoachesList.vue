2b3d92cb04da8d96149d5b294785ca4dfad31607
Artur Coelho <artur.rocha.coelho@gmail.com>
2022-05-25 23:13:40 -0300

    feat: added ui components to CoachItem and CoachesList

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,21 +1,34 @@
 <template>
   <section>Filter</section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register</router-link>
-    </div>
-    <ul v-if="hasCoaches">
-      <li v-for="coach in filteredCoaches" :key="coach.id">
-        {{ coach.firstName }}
-      </li>
-    </ul>
-    <h3 v-else>No coaches found.</h3>
+    <base-card>
+      <div class="controls">
+        <base-button mode="outline">Refresh</base-button>
+        <base-button link to="/register">Register</base-button>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :first-name="coach.firstName"
+          :last-name="coach.lastName"
+          :rate="coach.hourlyRate"
+          :areas="coach.areas"
+        ></coach-item>
+      </ul>
+      <h3 v-else>No coaches found.</h3>
+    </base-card>
   </section>
 </template>
 
 <script>
+  import CoachItem from '../../components/coaches/CoachItem.vue';
+
   export default {
+    components: {
+      CoachItem,
+    },
     computed: {
       filteredCoaches() {
         return this.$store.getters['coaches/coaches'];
@@ -26,3 +39,16 @@
     },
   };
 </script>
+
+<style scoped>
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  .controls {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
