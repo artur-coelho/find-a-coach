4b5a53290069a5f28dff31d04934f8793c79dae5
Artur Coelho <artur.rocha.coelho@gmail.com>
2022-07-07 21:38:15 -0300

    fetching coach data

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -5,7 +5,7 @@
   <section>
     <base-card>
       <div class="controls">
-        <base-button mode="outline">Refresh</base-button>
+        <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
         <base-button v-if="!isCoach" link to="/register">Register</base-button>
       </div>
       <ul v-if="hasCoaches">
@@ -65,9 +65,15 @@
         return this.$store.getters['coaches/isCoach'];
       },
     },
+    created() {
+      this.loadCoaches();
+    },
     methods: {
       setFilter(updatedFilters) {
         this.activeFilters = updatedFilters;
+      },
+      loadCoaches() {
+        this.$store.dispatch('coaches/loadCoaches');
       },
     },
   };
