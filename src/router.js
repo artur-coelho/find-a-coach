fac25f43aa6f506f67fc771900031b648f5cf8a0
Artur Coelho <artur.rocha.coelho@gmail.com>
2022-08-15 19:17:22 -0300

    starting auth page

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import CoachDetails from './pages/coaches/CoachDetails.vue';
 import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import Requests from './pages/requests/RequestsReceived.vue';
+import UserAuth from './pages/auth/UserAuth.vue';
 import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
@@ -20,6 +21,7 @@ const router = createRouter({
     },
     { path: '/register', component: CoachRegistration },
     { path: '/requests', component: Requests },
+    { path: '/auth', component: UserAuth },
     { path: '/:notFound(.*)', component: NotFound },
   ],
 });
