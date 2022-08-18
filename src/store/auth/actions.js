b4cfc9a123527529f6dc6c2bd32b691ab607a4c5
Artur Coelho <artur.rocha.coelho@gmail.com>
2022-08-18 13:56:19 -0300

    created authentication store

---
+++ src/store/auth/actions.js
@@ -0,0 +1,32 @@
+export default {
+  async signUp(context, payload) {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBEw2GBKh4wBjhceP_WaTnTrAMS4-P_dD8',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log(responseData);
+      const error = new Error(
+        responseData.message || `Failed to authenticate. Check your login data.`
+      );
+      throw error;
+    }
+
+    console.log(responseData);
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
+  login() {},
+};
--- src/store/index.js
+++ src/store/index.js
@@ -2,21 +2,13 @@ import { createStore } from 'vuex';
 
 import coachesModule from './coaches/index.js';
 import requestsModule from './requests/index.js';
+import authModule from './auth/index.js';
 
 const store = createStore({
   modules: {
     coaches: coachesModule,
     requests: requestsModule,
-  },
-  state() {
-    return {
-      userId: 'c3',
-    };
-  },
-  getters: {
-    userId(state) {
-      return state.userId;
-    },
+    auth: authModule,
   },
 });
 
