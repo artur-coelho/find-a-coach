83a75922fae2ae19db777253acce7180e250108b
Artur Coelho <artur.rocha.coelho@gmail.com>
2022-09-15 17:10:25 -0300

    added "auto login"

--- src/store/auth/actions.js
+++ src/store/auth/actions.js
@@ -1,6 +1,23 @@
 export default {
   async signUp(context, payload) {
-    const baseUrl = process.env.VUE_APP_API_AUTH_SIGNUP;
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signup',
+    });
+  },
+  async login(context, payload) {
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signin',
+    });
+  },
+  async auth(context, payload) {
+    const mode = payload.mode;
+    const baseUrl =
+      mode === 'signup'
+        ? process.env.VUE_APP_API_AUTH_SIGNUP
+        : process.env.VUE_APP_API_AUTH_SIGNIN;
+
     const response = await fetch(baseUrl, {
       method: 'POST',
       body: JSON.stringify({
@@ -12,47 +29,32 @@ export default {
     const responseData = await response.json();
 
     if (!response.ok) {
-      console.log(responseData);
       const error = new Error(
         responseData.message || `Failed to authenticate. Check your login data.`
       );
       throw error;
     }
 
-    console.log(responseData);
+    localStorage.setItem('token', responseData.idToken);
+    localStorage.setItem('userId', responseData.localId);
+
     context.commit('setUser', {
       token: responseData.idToken,
       userId: responseData.localId,
       tokenExpiration: responseData.expiresIn,
     });
   },
-  async login(context, payload) {
-    const baseUrl = process.env.VUE_APP_API_AUTH_SIGNIN;
-    console.log(baseUrl);
-    const response = await fetch(baseUrl, {
-      method: 'POST',
-      body: JSON.stringify({
-        email: payload.email,
-        password: payload.password,
-        returnSecureToken: true,
-      }),
-    });
-    const responseData = await response.json();
+  tryLogin(context) {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
 
-    if (!response.ok) {
-      console.log(responseData);
-      const error = new Error(
-        responseData.message || `Failed to authenticate. Check your login data.`
-      );
-      throw error;
+    if (token && userId) {
+      context.commit('setUser', {
+        token: token,
+        userId: userId,
+        tokenExpiration: null,
+      });
     }
-
-    console.log(responseData);
-    context.commit('setUser', {
-      token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn,
-    });
   },
   logout(context) {
     context.commit('setUser', {
